fb7895ddea325c17d42a507aca0001c0eb151364
Akshay580 <75792864+Akshay580@users.noreply.github.com>
2020-12-29 13:01:26 +0530

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:/Users/nisha sunil/Desktop/string.py ===============
+Enter a stringmississippi
+string is mississippi
+{'m': 1, 'i': 4, 's': 4, 'p': 2}
+>>> 
+================ RESTART: C:/Users/nisha sunil/Desktop/string.py ===============
+Enter a stringhello hoe are you
+string is hello hoe are you
+{'h': 2, 'e': 3, 'l': 2, 'o': 3, ' ': 3, 'a': 1, 'r': 1, 'y': 1, 'u': 1}
+>>> 
